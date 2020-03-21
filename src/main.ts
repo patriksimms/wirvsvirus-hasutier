afab7391ea9076a1b2abc007beb92f07e2965967
Julius Schmidt <newceptiondev@googlemail.com>
2020-03-21 09:27:50 +0100

    Auto stash before rebase of "refs/heads/master"

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as hbs from 'hbs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+  );
+
+  hbs.registerPartials(join(__dirname, '../src/frontend/views/partials'));
+
+  app.useStaticAssets(join(__dirname, '../src/frontend', 'public'));
+  app.setBaseViewsDir(join(__dirname, '../src/frontend', 'views'));
+  app.setViewEngine('hbs');
+
   await app.listen(3000);
 }
+
 bootstrap();
