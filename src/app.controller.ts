9e58c20274c103de02b7b59554dcac2ee7196eaa
Julius Schmidt <newceptiondev@googlemail.com>
2020-03-21 13:20:24 +0100

    - Updated Layout for login.hbs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,7 +3,7 @@ import { Controller, Get, Render } from '@nestjs/common';
 @Controller()
 export class AppController {
   @Get()
-  @Render('register')
+  @Render('login')
   root() {
     return { message: 'Hello world!' };
   }
