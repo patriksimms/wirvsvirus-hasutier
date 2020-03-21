4f4da43c99771a05ab25e374381f0937e3a5a005
Patrik Simms <minf102898>
2020-03-21 14:54:22 +0100

    finished user profile FE

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,8 +3,30 @@ import { Controller, Get, Render } from '@nestjs/common';
 @Controller()
 export class AppController {
   @Get()
-  @Render('register')
+  @Render('userprofile')
   root() {
-    return { message: 'Hello world!' };
+    return {message: "hello world",
+      expierence:
+        ["gassi gehen", "futter kaufen"],
+      animals: [
+        {
+          name: "Felix",
+          animalPicture: "assets/img/alexander-else-foto.jpg"
+        },
+        {
+          name: "Rex",
+          animalPicture: "assets/img/alexander-else-foto.jpg"
+        },
+        {
+          name: "katze",
+          animalPicture: "assets/img/alexander-else-foto.jpg"
+        },
+        {
+          name: "hund",
+          animalPicture: "assets/img/alexander-else-foto.jpg"
+        }
+      ]
+    };
   }
 }
+
