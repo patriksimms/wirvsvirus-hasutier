63845b4d38159c36fbcd698bf431d77970e540d2
Lukas Retelsdorf <l.retelsdorf23@gmail.com>
2020-03-21 19:24:03 +0100

    fixed CSS

--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
-//import * as hbs from 'hbs';
+import * as hbs from 'hbs';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(
     AppModule,
   );
-/*
+
   hbs.registerPartials(join(__dirname, '../src/frontend/views/partials'));
   hbs.registerHelper("setVar", function(varName, varValue, options) {
     options.data.root[varName] = varValue;
   });
-*/
-  app.useStaticAssets(join(__dirname, '../src/frontend', 'public'));
+
+  app.useStaticAssets(join(__dirname, '../', 'public'));
   app.setBaseViewsDir(join(__dirname, '../src/frontend', 'views'));
-//  app.setViewEngine('hbs');
+  app.setViewEngine('hbs');
 
   await app.listen(3000);
 }
