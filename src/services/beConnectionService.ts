811b86476db9be334653982570292fcaeac025b7
Patrik Simms <minf102898>
2020-03-21 20:49:16 +0100

    added http client

--- src/services/beConnectionService.ts
+++ src/services/beConnectionService.ts
@@ -1,11 +1,15 @@
 import { HttpService, Injectable } from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class BeConnectionService {
 
   constructor(private httpService: HttpService) {
 
-    // getAllAnimals(): Observable<AxiosResponse<>>
+  }
+
+  getAllAnimals() {
+    return this.httpService.get('http://localhost/animals/types').pipe(map(response => response.data))
   }
 }
