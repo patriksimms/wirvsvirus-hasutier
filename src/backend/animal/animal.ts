ee1a8ae7270d74434ea58987c0da8dda01064012
Lukas Retelsdorf <l.retelsdorf23@gmail.com>
2020-03-21 10:34:13 +0100

    Added entities

--- src/backend/animal/animal.ts
+++ src/backend/animal/animal.ts
@@ -1,6 +1,12 @@
 import { type } from './animalType';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity()
 export class animal {
+
+  @PrimaryGeneratedColumn()
   id: string;
+
+  @Column()
   type: type;
 }
--- src/backend/user/user.ts
+++ src/backend/user/user.ts
@@ -1,7 +1,20 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+
+@Entity()
 export class user {
+  @PrimaryGeneratedColumn()
   id: string;
+
+  @Column()
   email: string;
+
+  @Column()
   name: string;
+
+  @Column()
   lastName: string;
+
+  @Column()
   phone: string;
 }
