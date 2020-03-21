689554bd6c4f35676291916392a9deb95c02ffc1
Julius Schmidt <newceptiondev@googlemail.com>
2020-03-21 16:32:06 +0100

    - Link to add Animal when creating Search

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,29 +3,8 @@ import { Controller, Get, Render } from '@nestjs/common';
 @Controller()
 export class AppController {
   @Get()
-  @Render('userprofile')
+  @Render('createSearch')
   root() {
-    return {message: "hello world",
-      expierence:
-        ["gassi gehen", "futter kaufen"],
-      animals: [
-        {
-          name: "Felix",
-          animalPicture: "assets/img/alexander-else-foto.jpg"
-        },
-        {
-          name: "Rex",
-          animalPicture: "assets/img/alexander-else-foto.jpg"
-        },
-        {
-          name: "katze",
-          animalPicture: "assets/img/alexander-else-foto.jpg"
-        },
-        {
-          name: "hund",
-          animalPicture: "assets/img/alexander-else-foto.jpg"
-        }
-      ]
-    };
+    return {message: "hello world"};
   }
 }
